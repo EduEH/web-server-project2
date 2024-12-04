fbfe17941014cb51d229886bca4489ed02781435
EduEH <eduardo_hirai@hotmail.com>
2024-12-04 10:32:51 -0800

    scripts added

--- myportfolio/app.js
+++ myportfolio/app.js
@@ -13,15 +13,35 @@ var app = express();
 var passport = require('passport'); 
 var session = require('express-session'); 
 var GoogleStrategy = require('passport-google-oauth20').Strategy; 
+
+app.use(session({secret: 'cat',
+    resave: false,
+    saveUnitialized: false,
+    cookie: {secure: false}
+})); 
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+
+
 passport.use(new GoogleStrategy({ 
 clientID: '683472913075-7a0lp319cnm7q9rk274rr788jv67mg42.apps.googleusercontent.com',
 clientSecret: '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', 
-callbackURL: 'https://test.eddhirai.online/auth/google/callback'}, 
+callbackURL: 'http://localhost:4000/auth/google/callback'}, 
 function(accessToken, refreshToken, profile, cb) { 
 cb(null, profile); 
 } 
 ));
 
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+      res.redirect('/?user=' + encodeURIComponent(JSON.stringify(req.user)));
+  }
+);
 
 app.use('/auth', auth); 
 
@@ -29,13 +49,10 @@ app.use('/auth', auth);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(session({secret: 'cat',
-    resave: false,
-    saveUnitialized: false,
-    cookie: {secure: false}
-})); 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
+
+
+
+
 passport.serializeUser(function(user, done) { 
 done(null, user); 
 });
@@ -52,7 +69,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- myportfolio/routes/index.js
+++ myportfolio/routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router(); 
 /* GET home page. */ 
 router.get('/', function(req, res, next) { 
-if(req.user && req.user.displayName){ 
-const displayName = req.user.displayName; 
-req.user.displayName = null; 
-res.render('users', { username: displayName }); 
-} else{ 
-res.render('index', { title: 'Web-Project2', salut:'please log in' }); 
-} 
+    if(req.user && req.user.displayName){ 
+    const displayName = req.user.displayName; 
+    req.user.displayName = null; 
+    res.render('index', { user: req.user }); 
+  } else{ 
+    res.render('index', { title: 'Web-Project2', salut:'please log in' }); 
+  } 
 }); 
 module.exports = router;
